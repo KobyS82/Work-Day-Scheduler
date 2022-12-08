4427ec1611468774fabc4b7b0db5b4e61ff57152
KobyS82 <kobysillito@gmail.com>
2022-12-07 21:54:31 -0600

    cleaned up code

--- Assets/script.js
+++ Assets/script.js
@@ -9,42 +9,19 @@ $(function () {
   var hourVal = now.format('h');
   var amOrPm = now.format('A');
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Adds text content to local storage when save button is clicked
   $('.saveBtn').click(function(event) {
     var parent = $(this).parent().attr('id');
     var textArea = $(this).siblings('.description').val();
     localStorage.setItem(parent, textArea);
   });
 
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
   // Tests and manipulates variables depending on the time of day
-  console.log(hourVal, amOrPm);
-  // DELETE LATER vvv
-  hourVal = 1;
-
   var valTime = parseInt(hourVal);
   if (amOrPm === 'PM'){
     valTime+=10;
   }
-  // Changes the time up
-  // DELETE LATER
-  $('#upBtn').click(function(event){
-    valTime+=1;
-    console.log(valTime);
-    rightTime()
-  });
-  $('#date-time').text(valTime);
+  
   // Changes color of boxes to represent past, current, and future with class name changes
   function rightTime(){
     // Repeats for each hr block
@@ -76,6 +53,7 @@ $(function () {
    
   };
  
+  // Ties the local storage to the time block textareas
   function rightText(){
     // Repeats for each text block
     $('.description').each(function () {
@@ -84,11 +62,11 @@ $(function () {
     })
   };
 
-  // Adds
+  // Adds the time to the top of the page
   $('#weekDay').text(weekDay);
   $('#currentDay').text(currentDay);
   $('#currentTime').text(theTime);
 
   rightTime();
-  rightText()
+  rightText();
 });
---
+++ README.md
@@ -0,0 +1,34 @@
+# Work Day Scheduler
+
+## Description
+
+This page will serve as a schedule for a user.
+
+## Table of contents
+
+-[Header](#display-3)
+
+-[The time](#weekDay)
+
+-[Start of schedule](#hour-9)
+
+
+## Installation
+
+This page shows the coding repository: https://github.com/KobyS82/Work-Day-Scheduler
+
+
+## Usage
+
+This page can be viewed live at https://kobys82.github.io/Work-Day-Scheduler/
+
+![screenshot of content](.assets/images/test.png "This is a screenshot of the content") 
+
+
+## Credits
+
+Name, link, for
+
+Name, link, for
+
+Name, link, for
